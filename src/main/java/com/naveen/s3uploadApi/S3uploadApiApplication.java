b9d167de6570dc74d88f1e1cbffce4877bb4fd30
Naveen <naveen.chaurasia861@gmail.com>
2022-03-15 12:55:51 +0530

    first commit

---
+++ src/main/java/com/naveen/s3uploadApi/S3uploadApiApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.s3uploadApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3uploadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3uploadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naveen/s3uploadApi/S3uploadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.s3uploadApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3uploadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
